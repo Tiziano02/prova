6015cf672e33f1c83d9c6e4d243baf7fc100b147
Tiziano02 <tizianocostantiniw@icloud.com>
2022-05-10 23:12:35 +0200

    a

--- javascript/generale.js
+++ javascript/generale.js
@@ -1,13 +1,5 @@
 function prenota() {
   alert("hai premuto prenota ora");
 }
-function on() {
-  document.getElementById("overlay").style.display = "block";
-}
 
-function off() {
-  document.getElementById("overlay").style.display = "none";
-}
-$(document).ready(function () {
-  $('.carousel').carousel();
-});
+
